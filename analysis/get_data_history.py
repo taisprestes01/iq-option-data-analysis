88a3285feb8a391901b5e5b140acb970bacd9f95
Tais Prestes <108246691+taisprestes01@users.noreply.github.com>
2024-02-18 15:00:38 -0300

    refact: : python writing pattern

--- analysis/get_data_history.py
+++ analysis/get_data_history.py
@@ -2,7 +2,7 @@ from iqoptionapi.stable_api import IQ_Option
 import logging
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 
-def getHistory(login, password, count, mode):
+def get_history(login, password, count, mode):
     I_want_money=IQ_Option(login, password)
     I_want_money.connect()
     I_want_money.change_balance(mode)
